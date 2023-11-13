61840a0124971c0ba0dd08faef3cb0e8b632da70
Aaron Murray <plugins@omv-extras.org>
2023-11-13 15:58:53 -0600

    add migration script for local server and local ip fields

---
+++ usr/share/openmediavault/confdb/migrations.d/conf.service.wireguard_6.3.7.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+#
+# @license   http://www.gnu.org/licenses/gpl.html GPL Version 3
+# @author    Volker Theile <volker.theile@openmediavault.org>
+# @author    OpenMediaVault Plugin Developers <plugins@omv-extras.org>
+# @copyright Copyright (c) 2009-2013 Volker Theile
+# @copyright Copyright (c) 2013-2023 OpenMediaVault Plugin Developers
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+set -e
+
+. /usr/share/openmediavault/scripts/helper-functions
+
+xpath="/config/services/wireguard/clients/client"
+
+xmlstarlet sel -t -m "${xpath}" -v "uuid" -n ${OMV_CONFIG_FILE} |
+    xmlstarlet unesc |
+    while read uuid; do
+        if ! omv_config_exists "${xpath}[uuid='${uuid}']/localip"; then
+            omv_config_add_key "${xpath}[uuid='${uuid}']" "localip" "0"
+        fi
+    done;
+
+xpath="/config/services/wireguard/tunnels/tunnel"
+
+xmlstarlet sel -t -m "${xpath}" -v "uuid" -n ${OMV_CONFIG_FILE} |
+    xmlstarlet unesc |
+    while read uuid; do
+        if ! omv_config_exists "${xpath}[uuid='${uuid}']/localserver"; then
+            omv_config_add_key "${xpath}[uuid='${uuid}']" "localserver" ""
+        fi
+    done;
+
+exit 0
